aed40a8a472bbcee05402d1956bc0b5e5148b423
IanChak <60276491+IanTimchak@users.noreply.github.com>
2024-09-30 14:07:09 -0400

    Clarified header and status information for response methods

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -2,13 +2,15 @@ public_path = 'public/';
 
 module.exports = {
     okayuView: (req, res) => {
-        console.log("huh");
+        res.status(200); //OK
+        res.set('Content-Type', 'image/png');
         res.sendFile('images/okayufull.png', {
             root: public_path
         });
     },
     ascendView: (req, res) => {
-        console.log("huh");
+        res.status(200); //OK
+        res.set('Content-Type', 'image/png');
         res.sendFile('images/ascend.png', {
             root: public_path
         });
