70a9a208269b0f069e452d0dc4b68c03d801c7a5
Bryan <bryan@caskey.xyz>
2024-10-31 11:38:53 -0400

    Fixed createListing. Added findOneListingByKeyValue and closeConnection. Working on updateListing.  *userAccountController createAccount was altered for testing.

--- src/models/mongo.js
+++ src/models/mongo.js
@@ -1,15 +1,17 @@
-const {MongoClient, Collection} = require('mongodb');
+const {
+    MongoClient,
+    Collection
+} = require('mongodb');
 const mongoose = require('mongoose');
 var client;
 var Admin = mongoose.mongo.Admin;
 
 // Importing Schemas
-const accountSchema = require('src/models/accountSchema');
-const Account = mongoose.model('Account', accountSchema);
+const accountSchema = require('../models/accountSchema');
 
 startConnection().catch(err => console.log(err));
 
-async function startConnection(){
+async function startConnection() {
     /**
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
@@ -31,17 +33,17 @@ async function startConnection(){
     */
 }
 
-async function listDatabases(){
+async function listDatabases() {
     /*
     Mongoose doesn't seem to have any methods for directly getting the Databases list from the connection, so I 
     am using "Mongoose.mongo.admin" to use a list databases function I found here. It should work the same as before.
         - Bryan
     */
 
-    new Admin(mongoose.db).listDatabases(function(err, result) {
+    new Admin(mongoose.db).listDatabases(function (err, result) {
         console.log('listDatabases succeeded');
         // database list stored in result.databases
-        var databasesList = result.databases;    
+        var databasesList = result.databases;
         console.log("Databases:");
         databasesList.databases.forEach(db => console.log(` - ${db.name}`));
     });
@@ -52,16 +54,27 @@ async function listDatabases(){
 
 
 //Post 
-async function createListing(dbName, collection, newListing){
+async function createListing(dbName, collection, newListing) {
+    /*
+    The way this i sset up is that th eswitch statement switches on the name of the collection
+    provided. If it is a valid collection, it attempts to create a document in that collection.
+    If not, it uses the switch default and outputs that the collection name is incorrect.
+    Currently I am unsure if this is handling errors correctly, but it does create documents.
+        - Bryan
+    */
     console.log(newListing);
+    mongoose.connection.dbName = dbName; // Sets the database used by the mongoose connection
 
-    switch(collection) {
+    switch (collection) {
         case "accounts":
+            const Account = mongoose.model('Account', accountSchema.accountSchema);
             const doc = new Account(newListing);
             await doc.save();
-            console.log('New listing created: ' + doc);
+            console.log('New listing created: ', doc);
+            return doc._id;
         default:
             console.log('Collection not recognized in src/models/mongo.js');
+            return false;
     }
 
 
@@ -80,6 +93,22 @@ async function createListing(dbName, collection, newListing){
 }
 
 async function findOneListingByKeyValue(dbName, collection, nameOfListing) {
+    mongoose.connection.dbName = dbName;
+
+    switch (collection) {
+        case "accounts":
+            const Account = mongoose.model('Account', accountSchema.accountSchema);
+            Account.findById(nameOfListing, function (err, doc) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Found document matching key: ", nameOfListing);
+                    return doc;
+                }
+            });
+    }
+
+    /*
     const result = await client.db(dbName).collection(collection).findOne({ username: nameOfListing });
 
     if (result) {
@@ -89,25 +118,66 @@ async function findOneListingByKeyValue(dbName, collection, nameOfListing) {
         console.log(`No listings found with the name '${nameOfListing}'`);
         return (undefined)
     }
+    */
 }
 
-async function updateListingByKey(dbName, collection, listingKey, updatedListing) {
+async function updateListingByKey(dbName, collection, listingKey, updatedListing, doUpsert) {
+    mongoose.connection.dbName = dbName;
+
+    switch (collection) {
+        case "accounts":
+            const Account = mongoose.model('Account', accountSchema.accountSchema);
+            Account.findById(nameOfListing, function (err, doc) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Found document matching key: ", nameOfListing);
+                    return doc;
+                }
+            });
+    }
+    
+    /*
     const result = await client.db("route_mngt").collection("users")
-                        .updateOne({ Name: nameOfListing }, { $set: updatedListing });
+        .updateOne({
+            Name: nameOfListing
+        }, {
+            $set: updatedListing
+        });
 
     console.log(`${result.matchedCount} document(s) matched the query criteria.`);
     console.log(`${result.modifiedCount} document(s) was/were updated.`);
+    */
 }
 
 async function deleteListingByKey(dbName, collection, listingKey) {
+    mongoose.connection.dbName = dbName;
+
+    switch (collection) {
+        case "accounts":
+            const Account = mongoose.model('Account', accountSchema.accountSchema);
+            Account.findById(nameOfListing, function (err, doc) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Found document matching key: ", nameOfListing);
+                    return doc;
+                }
+            });
+
+    }
+
+    /*
     const result = await client.db("route_mngt").collection("users")
             .deleteOne({ Name: nameOfListing });
     console.log(`${result.deletedCount} document(s) was/were deleted.`);
+    */
 }
 
 async function closeConnection() {
-    console.log(`Closing Connection to ${client}`);
-    await client.close();
+    // Essentially the same as the standard mongo function.
+    console.log(`Closing Connection to ${mongoose.connection}`);
+    await mongoose.connection.close();
 }
 
 module.exports = {
