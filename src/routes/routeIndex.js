c460db8d77631ca4b0e0be97c5487280c65fa0d0
Ian Timchak <iantimchak0518@gmail.com>
2024-11-13 13:29:43 -0500

    fixed duplicate declaration of loginformcontroller

--- src/routes/routeIndex.js
+++ src/routes/routeIndex.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const homePageController = require('../controllers/homePageController');
 const loginFormController = require('../controllers/loginFormController');
-const loginFormController = require('../controllers/loginFormController');
 
 const router = express.Router();
 router.get('/', homePageController.homepageView);
