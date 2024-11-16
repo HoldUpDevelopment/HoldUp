4e827fa241976404dd6928434049bd2e540296bd
IanChak <60276491+IanTimchak@users.noreply.github.com>
2024-11-16 02:10:19 -0500

    reformatted login and singup, installed dotenv, installed jwt, login is now a post, changed html of login a bit.

--- public/js/login.js
+++ public/js/login.js
@@ -1,47 +1,45 @@
+var submitSpinner = `<div class="spinner-border text-light" role="status">
+                <span class="visually-hidden">Loading...</span>
+                </div>`;
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
 
 async function loginHTTP(username, password) {
     var xmlHttp = new XMLHttpRequest();
-    await xmlHttp.open("GET", `${origin}/api/user/login?userIdentifier=${username}&password=${password}`, false); // false for synchronous request
-    xmlHttp.send(null);
+    await xmlHttp.open("POST", `${origin}/api/user/login`, false); // false for synchronous request
+    xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlHttp.send(JSON.stringify({userIdentifier: username, password: password}));
     return {
         status: xmlHttp.status,
-        body: xmlHttp.responseText.toString()
+        body: xmlHttp.response
     };
 }
 
 //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 
-//Email validation
-function isEmail(string) {
-    //holy email regex
-    var re = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])/;
-    return re.test(string);
-}
-
-//////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////
-
 async function submitLogin(validityReference) {
+    $("#submit-button").html(submitSpinner);
     var loginRequest = await loginHTTP($("#username-input").val(), $("#password-input").val());
 
-    if (loginRequest.status == 200) {
+    if (loginRequest.status == 201) { //Success
+        validityReference.isValid = true;
+
+        //Bootstrap form styling
         $("#password-input").removeClass('is-invalid');
         $("#username-input").removeClass('is-invalid');
         $("#password-input").addClass('is-valid');
         $("#username-input").addClass('is-valid');
-        alert("Correct Password!");
-        validityReference.isValid = true;
-    } else if (loginRequest.status == 404) {
+        console.log(JSON.parse(loginRequest.body).message);
+    } else if (loginRequest.status == 401) { //Authentification failed
         validityReference.isValid = false;
-        $("#password-input").removeClass('is-invalid');
+
+        //Bootstrap form styling
+        $("#submit-button").html(`Login`);
         $("#username-input").addClass('is-invalid');
-        console.log(loginRequest.body);
-    } else {
-        validityReference.isValid = false;
-        $("#username-input").removeClass('is-invalid');
         $("#password-input").addClass('is-invalid');
-        console.log(loginRequest.body);
+        console.log(JSON.parse(loginRequest.body).error);
     }
 }
 
@@ -52,10 +50,7 @@ async function submitLogin(validityReference) {
 (() => {
     'use strict'
 
-    //////////////////////////////////////////////////////////////////
-
     const formFields = document.querySelectorAll('.form-validator');
-    //Sign Up Form Submission
     var form = document.getElementById("login-form");//This is using JQuery functionality. Jquery must be loaded into the HTML for it to compile
     form.addEventListener('submit', async event => { //Submit event listener for form
         var validityReference = { isValid: true };
@@ -70,18 +65,14 @@ async function submitLogin(validityReference) {
 
         //Login
         await submitLogin(validityReference);
-
         if (!validityReference.isValid) {
             console.log("NOT VALID");
             event.preventDefault();
             event.stopPropagation();
         } else {
             event.preventDefault();
-            
             window.location.href = `${origin}/dashboard`; //Navigate to the login page!
         }
         //form.classList.add('was-validated');
     }, false)
-
-    //////////////////////////////////////////////////////////////////
 })();
