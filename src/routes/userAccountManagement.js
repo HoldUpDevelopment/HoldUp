bb13b0d4d9f8661c49bc1cf46a9c94a912c4585d
IanChak <60276491+IanTimchak@users.noreply.github.com>
2024-11-18 19:01:14 -0500

    Adjusted user router

--- src/routes/userAccountManagement.js
+++ src/routes/userAccountManagement.js
@@ -3,8 +3,6 @@ const userAccountController = require('../controllers/userAccountController');
 
 const router = express.Router();
 router.post('/createAccount', userAccountController.createAccount); //Deprecated
-router.post('/signup', userAccountController.signup); //Uses urlencoded middleware
-router.post('/login', express.json(), userAccountController.login); //Uses json middleware
 router.delete('/deleteAccount', userAccountController.deleteAccount);
 router.put('/editAccountDetails', userAccountController.editAccountDetails);
 router.get('/getUserIdFromUserName', userAccountController.getUserIdFromUserName);
